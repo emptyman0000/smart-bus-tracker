fd2c5b1670e763499b8535a683ff4f1c29609beb
Võ Đức Hoàng Khánh <166082398+emptyman0000@users.noreply.github.com>
2025-05-29 07:48:53 +0700

    Update student.c

--- src/common/student.c
+++ src/common/student.c
@@ -14,11 +14,11 @@ static student_t* p_student_list;
 
 /* Init list of students */
 student_t student_list[15] = {
-    { 1, "Hoang_Khanh", "+84372385670", {0x63, 0xCC, 0xB2, 0x93}, CHECKOUT_HOME, {.data = student_1, .width = 220, .height = 220} },
-    { 2, "Duy_Khanh"  , "+84838469498", {0xF3, 0x6B, 0x65, 0xFC}, CHECKOUT_HOME, {.data = student_2, .width = 220,  .height = 220} },
-    { 3, "Van_Tin"    , "+84369538476", {0x42, 0x09, 0x7F, 0x4C}, CHECKOUT_HOME, {.data = student_3, .width = 220, .height = 220} },
-    { 4, "Quoc_Trung" , "+84123421342", {0x2A, 0x80, 0x67, 0x1A}, CHECKOUT_HOME, {.data = student_4, .width = 220, .height = 220} },
-    { 5, "Viet_Dung",   "+84345435434", {0xB3, 0x97, 0xC3, 0xED}, CHECKOUT_HOME, {.data = student_5, .width = 220, .height = 220} },
+    { 1, "Hoang_Khanh", "+84xxxxxxxxx", {0x63, 0xCC, 0xB2, 0x93}, CHECKOUT_HOME, {.data = student_1, .width = 220, .height = 220} },
+    { 2, "Duy_Khanh"  , "+84xxxxxxxxx", {0xF3, 0x6B, 0x65, 0xFC}, CHECKOUT_HOME, {.data = student_2, .width = 220,  .height = 220} },
+    { 3, "Van_Tin"    , "+84xxxxxxxxx", {0x42, 0x09, 0x7F, 0x4C}, CHECKOUT_HOME, {.data = student_3, .width = 220, .height = 220} },
+    { 4, "Quoc_Trung" , "+84xxxxxxxxx", {0x2A, 0x80, 0x67, 0x1A}, CHECKOUT_HOME, {.data = student_4, .width = 220, .height = 220} },
+    { 5, "Viet_Dung",   "+84xxxxxxxxx", {0xB3, 0x97, 0xC3, 0xED}, CHECKOUT_HOME, {.data = student_5, .width = 220, .height = 220} },
 };
 
 uint8_t STUDENT_TOTAL = sizeof(student_list) / sizeof(student_list[0]);
